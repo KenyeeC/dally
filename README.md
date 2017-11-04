9b0b933f53dbd1961a139d4bb9ed8f00671db3d8
KenyeeC <285482599@qq.com>
2017-11-04 12:48:00 +0800

    support port change

--- README.md
+++ README.md
@@ -40,3 +40,21 @@ Easy to render your html file on server
 ```
   dally --help
 ```
+
+## 使用方法
+
+- 全局安装dally
+> npm i -g dally
+
+- 然后进入到前端项目的目录下，执行以下命令即可
+
+  默认渲染index.html和监听3000端口，若端口已被占用则会自动监听别的端口
+  > dally
+
+  如果需要渲染指定的html，如example.html，则可以进行如下操作
+  
+  - 输入需要渲染的html名字:
+  > dally example
+
+  - 或直接在URL中输入对应的html：
+  > http://127.0.0.1:3000/example.html
--- logic.js
+++ logic.js
@@ -16,18 +16,45 @@ module.exports = function(argv, destinationPath){
     res.sendFile( path.join(destinationPath,htmlName+'.html'))
   })
   
+  app.use(function(err, req, res, next) {
+    console.error('err:',err.stack);
+    res.status(500).send('Something broke!');
+  })
+
   // default to port 3000
   var port = argv.port || 3000
   port = isNaN(Number(port)) ? 3000 : Number(port)
 
-  var server = app.listen(port, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log('#  \x1B[33m%s\x1b[0m','dally server start')
-    console.log('#  listening on port: \x1B[33m%s\x1b[0m', port)
-    console.log('#  render: \x1B[33m%s\x1b[0m', htmlName+'.html')
-    console.log('#  static path: \x1B[33m%s\x1b[0m', destinationPath)
-  });
+  var server = listen(app, port)
+
+  server.on('error', errHandler)
+
+  // auto to use new port
+  function errHandler (err) {
+    var code = err.code
+    switch (code) {
+      case 'EADDRINUSE':
+        var newPort = err.port + 1
+        console.log('#  \x1B[31mPort', err.port, 'is already in used !\x1B[39m')
+        console.log('#  \x1B[32mChange to use port:', newPort, '\x1B[39m')
+        var newServer = listen(app, newPort)
+        newServer.on('error', errHandler)
+        break
+      default:
+        break
+    }
+  }
+
+  // start server
+  function listen (app, port) {
+    return app.listen(port, function () {
+      console.log('#  \x1B[36m%s\x1B[0m','DALLY SERVER START:')
+      console.log('#  listen on port: \x1B[33m%s\x1b[0m', port)
+      console.log('#  render: \x1B[33m%s\x1b[0m', htmlName+'.html')
+      console.log('#  static path: \x1B[33m%s\x1b[0m', destinationPath)
+      console.log('#  Need help ? Entry: \x1B[33m%s\x1b[0m', 'dally --help')
+    })
+  }
 
 }
 
